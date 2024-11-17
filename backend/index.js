3ce6a2e2a448086074daa9de5414e64f946bd08e
ayoopriyanshu <112677512+ayoopriyanshu@users.noreply.github.com>
2024-11-18 00:36:31 +0530

    deployed the app live

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './db/config.js';
 import contactRoutes from './routes/contact.js';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 connectDB();
@@ -11,7 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
 app.use('/api/contacts', contactRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
